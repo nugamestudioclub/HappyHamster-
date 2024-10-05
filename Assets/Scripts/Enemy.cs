86d74aa20d802c8708fa23af2939db9d4494fed0
isaacspool <spool.i@northeastern.edu>
2024-10-05 14:38:56 -0400

    Added more enemy AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,21 @@ public class Enemy : MonoBehaviour
 {
 
     public GameObject player;
+
+    Vector2 _target = Vector2.zero;
+
+    const int MAP_SIZE = 57;
+    const int PLAYER_SAFE_DISTANCE = 10;
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void SetRandomTarget() {
+        _target = new Vector2(Random.Range(-MAP_SIZE, MAP_SIZE), Random.Range(-MAP_SIZE, MAP_SIZE));
     }
 
     // Update is called once per frame
@@ -17,13 +28,27 @@ public class Enemy : MonoBehaviour
     {
         // Move away from player
         Vector2 direction = transform.position - player.transform.position;
-        direction.Normalize();
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime;
+        if (direction.magnitude < PLAYER_SAFE_DISTANCE) {
+            direction.Normalize();
+            transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime;
+        } else {
+            // Move towards random point
+            Vector2 directionToTarget = _target - new Vector2(transform.position.x, transform.position.y);
+            if (directionToTarget.magnitude < 1) { // Find a new random target on the map to go to
+                SetRandomTarget();
+                while (Vector2.Distance(_target, player.transform.position) < PLAYER_SAFE_DISTANCE) {
+                    SetRandomTarget();
+                }
+            } else {
+                directionToTarget.Normalize();
+                transform.position += new Vector3(directionToTarget.x, directionToTarget.y, 0) * speed * Time.deltaTime;
+            }
+        }
     }
 
     public void Kill()
     {
-        // TODO: Death vfx and animations
+        // TODO: Death vfx and animations and score
         Destroy(gameObject);
     }
 }
